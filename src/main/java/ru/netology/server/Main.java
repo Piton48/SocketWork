bdd6198eee1499ca396a26472ebed366df405bfb
Sergey Seliverstov <seliverstov243@mail.ru>
2023-04-29 15:20:21 +0300

    task complited

--- src/main/java/ru/netology/server/Main.java
+++ src/main/java/ru/netology/server/Main.java
@@ -8,19 +8,23 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
+
     public static void main(String[] args) {
         int port = 8080;
-        try (ServerSocket serverSocket = new ServerSocket(port)) {
-            try (Socket clientSocket = serverSocket.accept();
-                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            ) {
+        try (ServerSocket serverSocket = new ServerSocket(port);
+             Socket clientSocket = serverSocket.accept();
+             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+
+            out.println("What is your name?");
+
+            System.out.println("New connection accepted");
+
+            String name = in.readLine();
+
+            out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
 
 
-
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
